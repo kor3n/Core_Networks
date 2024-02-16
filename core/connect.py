c151910fa28b3558be8cca4771c04827a79a06a3
Joshua Boat <joshua_boat@next.co.uk>
2024-02-16 10:09:42 +0000

    Updated the core library to the latest version which includes palo devices. Changed the naming schema to better adhere to PEP8. Updated the nemiko exceptions.

--- core/connect.py
+++ core/connect.py
@@ -1,12 +1,12 @@
-'''Docstring'''
+"""Docstring"""
 import os
 from netmiko import ConnectHandler
 from netmiko import NetMikoTimeoutException
 from netmiko import NetMikoAuthenticationException
 
 
-class Network_Device():
-    '''docstring for Network_Device'''
+class NetworkDevice:
+    """docstring for Network_Device"""
 
     def __init__(self, ip, username, password, secret, port=22):
         self.ip = ip
@@ -15,9 +15,11 @@ class Network_Device():
         self.secret = secret
         self.port = port
         self.connected = None
-        templates = f"{os.path.dirname(os.path.abspath(__file__))}" \
+        templates = (
+            f"{os.path.dirname(os.path.abspath(__file__))}"
             "\\modules\\textfsm\\templates\\"
-        os.environ['NET_TEXTFSM'] = templates
+        )
+        os.environ["NET_TEXTFSM"] = templates
 
     def _connect_network_device(self, switch):
         try:
@@ -44,132 +46,149 @@ class Network_Device():
             raise e
 
 
-class Base_Commands(Network_Device):
+class BaseCommands(NetworkDevice):
     """docstring for Base_Commands"""
 
     def __init__(self, *args):
         super().__init__(*args)
 
     def disconnect(self):
-        '''Docstring'''
+        """Docstring"""
         super()._disconnect_network_device()
 
     def find_device_prompt(self):
-        '''Docstring'''
+        """Docstring"""
         super()._find_prompt()
 
 
-class Cisco_IOS(Base_Commands):
+class CiscoIOS(BaseCommands):
     """docstring for Cisco_IOS"""
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.device_type = 'cisco_ios'  # 'cisco_nxos'
+        self.device_type = "cisco_ios"  # 'cisco_nxos'
 
     def connect(self):
-        '''Docstring'''
+        """Docstring"""
         switch = {
-            'device_type': self.device_type,
-            'ip': self.ip,
-            'username': self.username,
-            'password': self.password,
-            'secret': self.secret,
-            'port': self.port
+            "device_type": self.device_type,
+            "ip": self.ip,
+            "username": self.username,
+            "password": self.password,
+            "secret": self.secret,
+            "port": self.port,
         }
         super()._connect_network_device(switch)
 
     def run_command(self, command, textfsm=False):
-        '''Docstring'''
+        """Docstring"""
         if textfsm is False:
-            return self.connected.send_command(command, expect_string='#')
+            return self.connected.send_command(command, expect_string="#")
         elif textfsm is True:
             return self.connected.send_command(command, use_textfsm=True)
 
-    def run_command_set(self, commands):
-        '''Docstring'''
-        return self.connected.send_config_set(commands)
-
-    def interface_running_config(self, interface, output='str'):
-        '''Docstring'''
-        command = f'show run int {interface}'
-        if output.lower() == 'str':
+    def interface_running_config(self, interface, output="str"):
+        """Docstring"""
+        command = f"show run int {interface}"
+        if output.lower() == "str":
             return self.connected.send_command(command)
-        elif output.lower() == 'list':
+        elif output.lower() == "list":
             return self.connected.send_command(command).splitlines()
 
 
-class Cisco_NXOS(Base_Commands):
+class CiscoNXOS(BaseCommands):
     """docstring for Nexus_Switch"""
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.device_type = 'cisco_nxos'  # 'cisco_nxos'
+        self.device_type = "cisco_nxos"
 
     def connect(self):
-        '''Docstring'''
+        """Docstring"""
         switch = {
-            'device_type': self.device_type,
-            'ip': self.ip,
-            'username': self.username,
-            'password': self.password,
-            'secret': self.secret,
-            'port': self.port
+            "device_type": self.device_type,
+            "ip": self.ip,
+            "username": self.username,
+            "password": self.password,
+            "secret": self.secret,
+            "port": self.port,
         }
         super()._connect_network_device(switch)
 
     def run_command(self, command, textfsm=False):
-        '''Docstring'''
+        """Docstring"""
         if textfsm is False:
-            return self.connected.send_command(command) #, expect_string='#')
+            return self.connected.send_command(command)
         elif textfsm is True:
             return self.connected.send_command(command, use_textfsm=True)
 
-    def run_command_set(self, commands):
-        '''Docstring'''
-        return self.connected.send_config_set(commands)
-
     def switch_vdc(self, vdc):
-        '''Docstring'''
-        command = [f'switchto vdc {vdc}',
-                   'terminal width 511',
-                   'terminal length 0']
+        """Docstring"""
+        command = [f"switchto vdc {vdc}",
+                   "terminal width 511", "terminal length 0"]
         return self.connected.send_config_set(command)
 
 
-class Cisco_ASA(Base_Commands):
+class CiscoASA(BaseCommands):
     """docstring for Cisco_ASA"""
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.device_type = 'cisco_asa'  # 'cisco_nxos'
+        self.device_type = "cisco_asa"
 
     def connect(self):
-        '''Docstring'''
+        """Docstring"""
         switch = {
-            'device_type': self.device_type,
-            'ip': self.ip,
-            'username': self.username,
-            'password': self.password,
-            'secret': self.secret,
-            'port': self.port
+            "device_type": self.device_type,
+            "ip": self.ip,
+            "username": self.username,
+            "password": self.password,
+            "secret": self.secret,
+            "port": self.port,
         }
         super()._connect_network_device(switch)
 
     def run_command(self, command, textfsm=False):
-        '''Docstring'''
+        """Docstring"""
         if textfsm is False:
             return self.connected.send_command(command)
         elif textfsm is True:
             return self.connected.send_command(command, use_textfsm=True)
 
-    def run_command_set(self, commands):
-        '''Docstring'''
-        return self.connected.send_config_set(commands)
-
-    def interface_running_config(self, interface, output='str'):
-        '''Docstring'''
-        command = f'show run int {interface}'
-        if output.lower() == 'str':
+    def interface_running_config(self, interface, output="str"):
+        """Docstring"""
+        command = f"show run int {interface}"
+        if output.lower() == "str":
             return self.connected.send_command(command)
-        elif output.lower() == 'list':
+        elif output.lower() == "list":
             return self.connected.send_command(command).splitlines()
+
+
+class PaloPANOS(BaseCommands):
+    """docstring for palo"""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.device_type = "paloalto_panos"
+
+    def connect(self):
+        """Docstring"""
+        switch = {
+            "device_type": self.device_type,
+            "ip": self.ip,
+            "username": self.username,
+            "password": self.password,
+            "port": self.port,
+        }
+        super()._connect_network_device(switch)
+
+    def run_command(self, command, set_command=False, textfsm=False):
+        """Docstring"""
+        if textfsm is False:
+            if set_command is True and isinstance(command, list) is True:
+                return self.connected.send_config_set(
+                    command, expect_string=r">")
+            else:
+                return self.connected.send_command(command, expect_string=r">")
+        elif textfsm is True:
+            return self.connected.send_command(command, use_textfsm=True)
--- example.py
+++ example.py
@@ -1,41 +1,45 @@
-from core.connect import Cisco_IOS
-from core.connect import Cisco_NXOS
-from core.connect import Cisco_ASA
+'''
+example.py to show how to connect to devices and use textfsm
+'''
+from core.connect import CiscoIOS
+from core.connect import CiscoNXOS
+from core.connect import CiscoASA
 
 
-def main_NXOS():
-    # Example Call for Cisco's Nexus Devices
-    device = Cisco_NXOS('IP', 'USERNAME', 'PASSWORD', 'ENABLE')
+def main_nxos() -> None:
+    '''Example Call for Cisco's Nexus Devices'''
+    device = CiscoNXOS('IP', 'USERNAME', 'PASSWORD', 'ENABLE')
     device.connect()
     device.run_command('show version', textfsm=True)
     device.disconnect()
 
 
-def main_IOS():
-    # Example Call for Cisco's Catalyst IOS Devices
-    device = Cisco_IOS('IP', 'USERNAME', 'PASSWORD', 'ENABLE')
+def main_ios() -> None:
+    '''Example Call for Cisco's Catalyst IOS Devices'''
+    device = CiscoIOS('IP', 'USERNAME', 'PASSWORD', 'ENABLE')
     device.connect()
     device.run_command('show version', textfsm=True)
     device.disconnect()
 
 
-def main_ASA():
-    # Example Call for Cisco's ASA Devices
-    device = Cisco_ASA('IP', 'USERNAME', 'PASSWORD', 'ENABLE')
+def main_asa() -> None:
+    '''Example Call for Cisco's ASA Devices'''
+    device = CiscoASA('IP', 'USERNAME', 'PASSWORD', 'ENABLE')
     device.connect()
     device.run_command('show version', textfsm=True)
     device.disconnect()
 
 
 def main():
+    '''Main Fucntion to call all example functions'''
     # Example Call for Cisco's Nexus Devices
-    main_NXOS()
+    main_nxos()
 
     # Example Call for Cisco's Catalyst IOS Devices
-    main_IOS()
+    main_ios()
 
     # Example Call for Cisco's ASA Devices
-    main_ASA()
+    main_asa()
 
 
 if __name__ == '__main__':
